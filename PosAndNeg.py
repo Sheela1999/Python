372deb89769cec6c1ff48829200a1628c9026624
Sheela1999 <sheela.xworkz@gmail.com>
2025-01-02 22:17:57 +0530

    Basics

---
+++ PosAndNeg.py
@@ -0,0 +1,17 @@
+n = -2
+
+if n > 0:
+    print("+ ve no")
+else :
+    print("-ve no")
+
+# print all odd numbers within a given range
+
+def rangeValue():
+
+      for i in range(2 , 20):
+          if i % 2 != 0:
+             print(i, end = " ") #end = " " for getting a output in one line 
+
+rangeValue()
+
